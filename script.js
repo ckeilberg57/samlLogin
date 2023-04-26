ef7f8952be19bfcb45b5ded76ce7e94be3d057d2
Chris Keilberg <83353741+ckeilberg57@users.noreply.github.com>
2023-04-26 12:26:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ form.onsubmit = (e) => {
 
     function checkEmail() {
         console.log("inside check email");
-        let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+        let pattern = /^[^ ];
         if (!eInput.value.match(pattern)) {
             eField.classList.add("error");
             eField.classList.remove("valid");
